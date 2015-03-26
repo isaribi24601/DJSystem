03e39935ce00980df058e01ca468a19191e85fa6
taketo957 <taketo957@gmail.com>
2015-03-26 22:10:49 +0900

    DBの最初の曲を再生できるようにした

--- app.rb
+++ app.rb
@@ -1,40 +1,41 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
-require 'json'
+require 'sinatra/json'
 
 require './models/songs.rb'
 
-    set :bind, '192.168.33.10'
-    set :port, 3000
+#    set :bind, '192.168.33.10'
+#    set :port, 3000
 
 
 get '/' do
-    'Hello Sintra!'
-    @contents = Song.order("id desc").all
-    erb :index
-    
+  'Hello Sintra!'
+  @contents = Song.order("id desc").all
+  erb :index
+
 end
 
 post '/dj' do
-    
-    logger.info "URL：#{params[:url]}"
+
+  logger.info "URL：#{params[:url]}"
   url = params[:url]
   # urlを分割して、IDだけを取り出す
-  
- video_id = url.split("=")[1]
- Song.create(:url => video_id)
 
-redirect '/'
-    
+  video_id = url.split("=")[1]
+  Song.create(:url => video_id)
+
+  redirect '/'
+
 end
 
 get '/api/dj' do
-    # Songから最初の１件を取り出す
-    data = Song.select('url').first
-    data.to_json
+  # Songから最初の１件を取り出す
+  data = Song.first[:url]
+  p data
+  data
 end
 
 
 
-    
+
