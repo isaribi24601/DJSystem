c370dce7ef28c2c9d65a82743c1a613b688113c5
vagrant <vagrant@vagrant-centos63.(none)>
2015-03-26 08:22:06 +0000

    there are still some error,but now I deploy it.

--- app.rb
+++ app.rb
@@ -1,6 +1,7 @@
 require 'sinatra'
 require 'sinatra/reloader'
 require 'active_record'
+require 'json'
 
 require './models/songs.rb'
 
@@ -10,6 +11,7 @@ require './models/songs.rb'
 
 get '/' do
     'Hello Sintra!'
+    @contents = Song.order("id desc").all
     erb :index
     
 end
@@ -17,10 +19,22 @@ end
 post '/dj' do
     
     logger.info "URL：#{params[:url]}"
-  Song.create(:url => params[:url])
+  url = params[:url]
+  # urlを分割して、IDだけを取り出す
+  
+ video_id = url.split("=")[1]
+ Song.create(:url => video_id)
 
 redirect '/'
     
 end
+
+get '/api/dj' do
+    # Songから最初の１件を取り出す
+    data = Song.select('url').first
+    data.to_json
+end
+
+
 
     
