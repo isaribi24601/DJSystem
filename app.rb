358077f8ed78eea8a83f37a466bf32097be12485
taketo957 <taketo957@gmail.com>
2015-03-27 15:17:48 +0900

    連続再生完了

--- app.rb
+++ app.rb
@@ -7,51 +7,51 @@ ActiveRecord::Base.configurations = YAML.load_file('database.yml')
 
 require './models/songs.rb'
 
-    set :bind, '192.168.33.10'
-    set :port, 3000
+#    set :bind, '192.168.33.10'
+#    set :port, 3000
 
 
 get '/' do
-    'Hello Sintra!'
-    @contents = Song.order("id desc").all
-    erb :index
-    
+  'Hello Sintra!'
+  @contents = Song.order("id desc").all
+  erb :index
+
 end
 
 get '/post' do
-    erb :post
-    
+  erb :post
+
 end
 
 post '/dj' do
-    logger.info "URL：#{params[:url]}"
+  logger.info "URL：#{params[:url]}"
   url = params[:url]
   # urlを分割して、IDだけを取り出す
 
- video_id = url.split("=")[1]
- Song.create(:url => video_id)
+  video_id = url.split("=")[1]
+  Song.create(:url => video_id)
 
-redirect '/post'
-    
+  redirect '/post'
+
 end
 
 get '/api/dj' do
-    # Songから最初の１件を取り出す
-   data = Song.first[:url]
-    p data
-    data
-    
+  # Songから最初の１件を取り出す
+  data = Song.first[:url]
+  Song.first.destroy
+  p data
+  data
 end
 
 post '/api/deletesong' do
-    
-    song = Song.where(url: params[:videoId]).first.destroy
-    
+
+  song = Song.where(url: params[:videoId]).first.destroy
+
 end
 
 get '/api/redirect' do
-    
-    redirect '/'
-    
+
+  redirect '/'
+
 end
-    
+
